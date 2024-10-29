763b743cfea5c144bdcc27006befb4248e3289d3
gazlv19 <gava4932@colorado.edu>
2024-10-29 10:07:36 -0600

    ica10

---
+++ ica/ica10.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById("name"));
+document.getElementById("name").addEventListener("click", function(e){
+    document.getElementById("name").innerHTML="short for Gazal";
+})
+document.getElementById("color").addEventListener("mouseover",function(e){
+    document.getElementById("color").style.color="pink";
+})
